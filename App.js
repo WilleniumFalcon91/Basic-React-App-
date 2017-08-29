2e9a506c875f6e78cd8cb458e9356db806856a95
William Elliott <wcelliott1991@gmail.com>
2017-08-29 16:27:36 -0400

    adds click functionality

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Readout from './Readout.js'
-import MagicButton from './MagicButton.js'
+
+import Readout from './Readout.js';
+import MagicButton from './MagicButton.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.theNumber = 999;
+    this.state = {
+      theNumber: 999
+    };
   }
   render() {
     return (
@@ -16,12 +19,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Will is awesome!</h2>
         </div>
-        <p className="App-intro">
-          It's true.
-        </p>
-        <MagicButton title="Press here for fun" />
+        {/* <p className="App-intro"> */}
+          <Readout unit="f" temperature={this.state.theNumber} />
+        {/* </p> */}
+        <MagicButton 
+          title="Press here for fun" 
+          handleClick={this._changeNumber}
+        />
       </div>
     );
+  }
+
+  _changeNumber = () => {
+    console.log('I am having the time of my life');
+    const newNumber = this.state.theNumber * this.state.theNumber;
+    this.setState({
+      theNumber: newNumber
+    });
+    // console.log(this.state.theNumber);
   }
 }
 
--- MagicButton.js
+++ MagicButton.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
-const MagicButton = (props) => (
-    <button>{props.title}</button>
+const MagicButton = ({
+    title, 
+    handleClick
+}) => (
+    <button onClick={handleClick}>{title}</button>
 );
+
+// const logIt = () => {
+//     console.log(`The button got pressed`)
+// };
 
 export default MagicButton;
